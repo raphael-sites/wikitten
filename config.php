668cace5198f5750aa73dea399561dd16e01298a
Shu <chugen.shu@weblio.co.jp>
2014-10-03 16:41:00 +0900

    -

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+// Example configuration file for Wikitten. To use it,
+// first rename it to `config.php`.
+
+// Custom name for your wiki:
+define('APP_NAME', 'RAPH');
+
+// Custom path to your wiki's library:
+// define('LIBRARY', '/path/to/wiki/library');
+
+// Enable editing files through the interface?
+// NOTE: There's currently no authentication built into Wikitten, controlling
+// who does what is your responsibility.
+// define('ENABLE_EDITING', true);
+
+// Enable JSON page data?
+// define('USE_PAGE_METADATA', true);
+// define('USE_WIKITTEN_LOGO', true);
+// 
+// Enable PasteBin plugin ?
+// define('ENABLE_PASTEBIN', true);
+// define('PASTEBIN_API_KEY', '');
